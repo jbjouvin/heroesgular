0ba0adf5253e36059a9fef5488aa62924b84873a
jbjouvin <jb.jouvin@gmail.com>
2017-08-29 16:08:30 +0200

    test heroes tutorial angular

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,15 +8,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
-        selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        selector: 'heroes-app',
+        template: "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 //# sourceMappingURL=app.component.js.map
